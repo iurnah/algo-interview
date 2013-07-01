0c4bee7c84e629a21dfa13fdddd8bcca09f67a7f
Rui Han/MBP <iurnah@gmail.com>
2013-06-30 23:53:19 -0700

    add the file sort.cpp

---
+++ sort.cpp
@@ -0,0 +1,16 @@
+/* This is a libry that including all the popular sorting algorithms
+ * for my job interviews. I prepare to make this a comprehensive file
+ * that include all the common used sorting codes
+ */
+
+#include <iostream>
+
+using namespace std;
+
+void qsort()
+{
+
+
+}
+
+
